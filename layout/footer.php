d4a04ee3759a8f1d4a99f79d878bb416440d3a3b
Nouster <djebali.mohamed88@gmail.com>
2023-02-27 17:49:19 +0100

    Added script link to bootstrap for dropdown menu

--- layout/footer.php
+++ layout/footer.php
@@ -1,19 +1,39 @@
-<footer class="container-fluid mt-5 bg-dark text-light fixed-bottom">
-    <div class="row justify-content-around py-3">
-        <div class="col-md-6 d-flex align-items-center">
-            <a class="text-uppercase text-decoration-none text-light fw-bold">My corp<br><span class="fw-light">©2023</span></a>
-        </div>
-        <div class="col-md-3">
-            <ul class="list-unstyled">
-                <li><a class="text-light text-decoration-none" href="#">Home</a></li>
-                <li><a class="text-light text-decoration-none" href="#">Link</a></li>
-                <li>
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <footer class="container-fluid mt-5 bg-dark text-light fixed-bottom">
+        <div class="row justify-content-around py-3">
+            <div class="col-md-6 d-flex align-items-center">
+                <a class="text-uppercase text-decoration-none text-light fw-bold">My corp<br><span class="fw-light">©2023</span>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <ul class="list-unstyled">
+                    <li>
+                        <a class="text-light text-decoration-none" href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="text-light text-decoration-none" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light text-decoration-none" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
-                    </a>
-                </li>
-            </ul>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Another action</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-</footer>
+    </footer>
 </body>
 </html>
