2970d5784306d7d6b3bba0d64aaf1567052900bd
Nouster <djebali.mohamed88@gmail.com>
2023-02-27 13:05:47 +0100

    Extract data array to data folder in leaderboard.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
-$leaderboard = [
-  [
-    'email' => 'if@nivwev.ni',
-    'score' => 816,
-  ],
-  [
-    'email' => 'diteris@hajkowpof.je',
-    'score' => 798,
-  ],
-  [
-    'email' => 'nahefun@zip.bz',
-    'score' => 793,
-  ],
-  [
-    'email' => 'zogputeh@roz.et',
-    'score' => 688,
-  ],
-  [
-    'email' => 'wa@tetaptaw.pl',
-    'score' => 530,
-  ],
-];
-?>
-<?php
+require_once 'data/leaderboard.php';
 // La fonction "array_map" prend en premier argument une fonction anonyme qui sera appliquée à chaque élément du tableau "$leaderboard". Cette fonction anonyme a un argument "$leaderboard" qui représente chaque joueur dans le tableau.
 
 $replaceChar = array_map(function($leaderboard) {
