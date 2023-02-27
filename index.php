db748fc3e000a3c44ef5c6f576c44457097912f9
facte sun <shems.chelgoui@gmail.com>
2023-02-27 21:28:03 +0100

    finito

--- index.php
+++ index.php
@@ -1,49 +1,12 @@
-<?php
-$leaderboard = [
-  [
-    'email' => 'if@nivwev.ni',
-    'score' => 816,
-  ],
-  [
-    'email' => 'diteris@hajkowpof.je',
-    'score' => 798,
-  ],
-  [
-    'email' => 'nahefun@zip.bz',
-    'score' => 793,
-  ],
-  [
-    'email' => 'zogputeh@roz.et',
-    'score' => 688,
-  ],
-  [
-    'email' => 'wa@tetaptaw.pl',
-    'score' => 530,
-  ],
-];
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Leaderboard</title>
-</head>
+<html>
+  <?php
+    require_once (__DIR__.'/assets/data/leaderboard.php');
+    require_once (__DIR__.'/assets/layout/head.php');
+  ?>
 
 <body>
 
-  <h1>Leaderboard</h1>
-
-  <?php foreach ($leaderboard as $player) { ?>
-    <div>
-      <?php echo $player['pseudo']; ?>
-      <br />
-      <?php echo $player['score']; ?>
-    </div>
-  <?php } ?>
+  <?php require_once (__DIR__.'/assets/templates/Leaderboard.php');?>
 
 </body>
 
-</html>
